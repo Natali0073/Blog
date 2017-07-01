eec7847b63fb79424e448dc8bf96d41893ab9280
Natali0073 <nataliamakarchuk17@gmail.com>
2017-07-01 12:29:07 +0300

    add styles and default messages

--- app/main.js
+++ app/main.js
@@ -1,35 +1,60 @@
-angular.module("myBlog", [])
-        .controller('myCtrl', myCtrl)
+angular
+  .module("myBlog", [])
+  .controller('myCtrl', myCtrl)
 
 myCtrl.$inject = ['$scope'];
 
 function myCtrl($scope){
-//     localStorage.clear();
+    // localStorage.clear();
     $scope.messageArray = [];
     $scope.disabled = true;
+
+    $scope.arrayMessages = [
+      {
+        userName: "Oleksa",
+        userMessage: "sit amet consectetur adipisicing elit of them jean shorts sed magna aliqua Lorem ipsum dolor met",
+        id: 0
+      },
+      {
+        userName: "Ihor",
+        userMessage: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        id: 1
+      },
+      {
+        userName: "Anonimus",
+        userMessage: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        id: 2
+      }
+
+    ];
+    $scope.arrayMessages.forEach(function(el){
+      var element = JSON.stringify(el);
+      localStorage.setItem(el.id, element);
+    })
     var id = Object.keys(localStorage).length;
-    
+
     $scope.save = function(){
         var blogMessage = {
-            name: $scope.name,
-            text: $scope.text
+            userName: $scope.userName,
+            userMessage: $scope.userMessage
         };
         var obj = JSON.stringify(blogMessage);
         id += 1;
         localStorage.setItem(id, obj);
-        $scope.text = '';
-        $scope.name = '';
+        $scope.userMessage = '';
+        $scope.userName = '';
 
         var messageObj = JSON.parse(localStorage.getItem(id));
-        $scope.messageArray.push(messageObj);
+        $scope.messageArray.unshift(messageObj);
     };
-  
+
     for(var key in localStorage){
         var messageObj = JSON.parse(localStorage.getItem(key));
         $scope.messageArray.push(messageObj);
     }
-    
+    $scope.messageArray.reverse();
+
     $scope.editMessage = function(){
-        
+
     }
 }
